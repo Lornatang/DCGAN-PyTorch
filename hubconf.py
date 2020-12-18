f5cfa09628b5205a89130499f40c658c0f27fa7b
Changyu <liuchangyu1111@gmail.com>
2020-12-18 17:47:30 +0800

    add Torch hub support

---
+++ hubconf.py
@@ -0,0 +1,67 @@
+# Copyright 2020 Dakewe Biotech Corporation. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""File for accessing GAN via PyTorch Hub https://pytorch.org/hub/
+Usage:
+    import torch
+    model = torch.hub.load("Lornatang/GAN-PyTorch", "mnist", pretrained=True, image_size=28, channels=1)
+"""
+from torch.hub import load_state_dict_from_url
+
+from dcgan_pytorch.models import Generator
+
+model_urls = {
+    "cifar10": "https://github.com/Lornatang/DCGAN-PyTorch/releases/download/0.1.0/cifar10-30241ac8.pth",
+    "cartoon": "https://github.com/Lornatang/DCGAN-PyTorch/releases/download/0.1.0/cartoon-c65fc503.pth"
+}
+
+dependencies = ["torch"]
+
+
+def create(arch, pretrained, progress):
+    """ Creates a specified GAN model
+
+    Args:
+        arch (str): Arch name of model.
+        pretrained (bool): Load pretrained weights into the model.
+        progress (bool): Show progress bar when downloading weights.
+
+    Returns:
+        PyTorch model.
+    """
+    model = Generator()
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls[arch], progress=progress)
+        model.load_state_dict(state_dict)
+    return model
+
+
+def cifar10(pretrained: bool = False, progress: bool = True) -> Generator:
+    r"""GAN model architecture from the
+    `"One weird trick..." <https://arxiv.org/abs/1511.06434>`_ paper.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return create("cifar10", pretrained, progress)
+
+
+def cartoon(pretrained: bool = False, progress: bool = True) -> Generator:
+    r"""GAN model architecture from the
+    `"One weird trick..." <https://arxiv.org/abs/1511.06434>`_ paper.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return create("cartoon", pretrained, progress)
