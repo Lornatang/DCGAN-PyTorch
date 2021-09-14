b0fb9345a03267a4cf84187f7338b087ede1c03d
Liu Changyu <liuchangyu1111@gmail.com>
2021-09-14 10:07:42 +0800

    Create model.py

---
+++ model.py
@@ -0,0 +1,101 @@
+# Copyright 2021 Dakewe Biotech Corporation. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+# ==============================================================================
+# File description: Realize the model definition function.
+# ==============================================================================
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+__all__ = [
+    "Discriminator", "Generator"
+]
+
+
+class Discriminator(nn.Module):
+    def __init__(self) -> None:
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+            # Input is 3 x 64 x 64
+            nn.Conv2d(3, 64, (4, 4), (2, 2), (1, 1), bias=True),
+            nn.LeakyReLU(0.2, True),
+            # State size. 64 x 32 x 32
+            nn.Conv2d(64, 128, (4, 4), (2, 2), (1, 1), bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, True),
+            # State size. 128 x 16 x 16
+            nn.Conv2d(128, 256, (4, 4), (2, 2), (1, 1), bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, True),
+            # State size. 256 x 8 x 8
+            nn.Conv2d(256, 512, (4, 4), (2, 2), (1, 1), bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, True),
+            # State size. 512 x 4 x 4
+            nn.Conv2d(512, 1, (4, 4), (1, 1), (0, 0), bias=True),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = self.main(x)
+        out = torch.flatten(out, 1)
+
+        return out
+
+
+class Generator(nn.Module):
+    def __init__(self) -> None:
+        super(Generator, self).__init__()
+        self.main = nn.Sequential(
+            # Input is 100, going into a convolution.
+            nn.ConvTranspose2d(100, 512, (4, 4), (1, 1), (0, 0), bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # state size. 512 x 4 x 4
+            nn.ConvTranspose2d(512, 256, (4, 4), (2, 2), (1, 1), bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            # state size. 256 x 8 x 8
+            nn.ConvTranspose2d(256, 128, (4, 4), (2, 2), (1, 1), bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            # state size. 128 x 16 x 16
+            nn.ConvTranspose2d(128, 64, (4, 4), (2, 2), (1, 1), bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            # state size. 64 x 32 x 32
+            nn.ConvTranspose2d(64, 3, (4, 4), (2, 2), (1, 1), bias=True),
+            nn.Tanh()
+            # state size. 3 x 64 x 64
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self._forward_impl(x)
+
+    # Support PyTorch.script function.
+    def _forward_impl(self, x: Tensor) -> Tensor:
+        out = self.main(x)
+
+        return out
+
+    def _initialize_weights(self) -> None:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                torch.nn.init.normal_(m.weight, 0.0, 0.02)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                torch.nn.init.normal_(m.weight, 1.0, 0.02)
+                torch.nn.init.zeros_(m.bias)
